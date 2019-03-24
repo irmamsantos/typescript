75769672f710946044690bcde276ae027c94f84f
Irma Santos <irma.santos@gmail.com>
2019-03-24 17:01:08 +0000

    estrutura

---
+++ helloword/helloword.js
@@ -0,0 +1,13 @@
+/*
+//javascript
+function dizerOla(nome) {
+    console.log('Olá ' + nome);
+}
+
+dizerOla('Algaworks');
+*/
+//typecript
+function dizerOla(nome) {
+    console.log("Ol\u00E1 " + nome);
+}
+dizerOla('Algaworks');
---
+++ helloword/helloword.ts
@@ -0,0 +1,15 @@
+/*
+//javascript
+function dizerOla(nome) {
+    console.log('Olá ' + nome);
+}
+
+dizerOla('Algaworks');
+*/
+
+//typecript
+function dizerOla(nome: string) {
+    console.log(`Olá ${nome}`);
+}
+
+dizerOla('Algaworks');
